e7f9b25c8e8dbb1a2b6bcca062d6c73193602a6d
jj811208 <jj811208@gmail.com>
2021-06-02 18:11:56 +0800

    init

---
+++ data.js
@@ -0,0 +1,85 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "沙灘",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 400,
+      "initialViewParameters": {
+        "yaw": 0.5289690194505887,
+        "pitch": 0.0074239512117344475,
+        "fov": 1.3808549253827036
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8109520529820813,
+          "pitch": -0.11133323913712267,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.14364526015437562,
+          "pitch": 0.20407019960490125,
+          "title": "很多沙子",
+          "text": "儼然就是沙灘"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "屋子",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 400,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.556966011292573,
+          "pitch": 0.12789673005724467,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.00020453361324257457,
+          "pitch": 0,
+          "title": "很白的點",
+          "text": "白白白"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
